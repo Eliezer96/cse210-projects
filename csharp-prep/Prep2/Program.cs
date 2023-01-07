bbe7a9c2a6f0e888e460534b0737504a603d8af8
Eliezer <joseprivera625@gmail.com>
2023-01-06 23:26:21 -0600

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter you grade: ");
+        String answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        String letter = "";
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+         
+        else
+        {
+            letter = "F";
+        }
+
+
+        String sign = "";
+        if (grade % 10 >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (grade % 10 < 3)
+        {
+            sign = "-";
+        }
+
+        else{
+            sign = "";
+        }
+        
+        Console.WriteLine($"Your letter is {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation! You passed.");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+         
+         
     }
 }
