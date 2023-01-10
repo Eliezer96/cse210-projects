2e8ee8f184b95ce116a67a6bb6e92ae7ba28bfba
Eliezer <joseprivera625@gmail.com>
2023-01-09 23:13:27 -0600

    Finished pre3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int secretNumber = randomGenerator.Next(1, 101);
+        //Console.WriteLine(secretNumber);
+
+        int Guess = -1;
+
+        while (secretNumber != Guess)
+        {
+            
+            Console.Write("Enter your guess: ");
+            String guess = Console.ReadLine();
+            Guess = int.Parse(guess);
+
+            if (Guess > secretNumber)
+            {
+                Console.WriteLine("Go lower");
+            }
+
+            else if (Guess < secretNumber)
+            {
+                Console.WriteLine("Go higher");
+            }
+
+            else 
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
+
+        }
+
+        
     }
 }
